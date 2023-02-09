31c904560dec4e0ff7580f158a2a132d706a61d2
zeritaken <124591496+zeritaken@users.noreply.github.com>
2023-02-09 08:35:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,64 @@
 import os
-import logging
-from telegram import ParseMode
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    ConversationHandler,
-    PicklePersistence,
-)
-from utils import handlers
+from telegram.ext import Updater, CommandHandler, MessageHandler, InlineQueryHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 
-# Ajoutez un niveau de journalisation approprié pour Heroku
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+token = os.environ.get("BOT_TOKEN", None)
 
-# Ajoutez un nom de fichier significatif pour la persistence
-persistence = PicklePersistence("conversationbot_persistence")
-
-token = os.environ["BOT_TOKEN"]
-
-updater = Updater(token=token, use_context=True, persistence=persistence)
+updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
-# Utilisez des constantes pour les états au lieu de chaînes
-PROCEED, FOLLOW_TELEGRAM, FOLLOW_TWITTER, FOLLOW_YOUTUBE, SUBMIT_ADDRESS, END_CONVERSATION, LOOP, SUREWANTTO, CAPTCHASTATE = range(9)
+# To catch commands like "/start"
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-states = {
-    PROCEED: [
-        MessageHandler(Filters.regex("^🚀 Join Airdrop$"), handlers.submit_details),
-        handlers.cancel_handler,
-    ],
-    FOLLOW_TELEGRAM: [
-        MessageHandler(Filters.regex("^Submit Details$"), handlers.follow_telegram),
-        handlers.cancel_handler,
-    ],
-    FOLLOW_TWITTER: [
-        MessageHandler(Filters.regex("^Done$"), handlers.follow_twitter),
-        handlers.cancel_handler,
-    ],
-    FOLLOW_YOUTUBE: [
-        MessageHandler(Filters.regex("^Done$"), handlers.follow_youtube),
-        handlers.cancel_handler,
-    ],
-    SUBMIT_ADDRESS: [handlers.cancel_handler, MessageHandler(Filters.text, handlers.submit_address)],
-    END_CONVERSATION: [
-        handlers.cancel_handler,
-        MessageHandler(Filters.regex("^0x[a-fA-F0-9]{40}$"), handlers.end_conversation),
-    ],
-    LOOP: [MessageHandler(Filters.text, handlers.loop_answer)],
-    SUREWANTTO: [MessageHandler(Filters.regex("^(YES|NO)$"), handlers.sure_want_to)],
-    CAPTCHASTATE: [MessageHandler(Filters.text, handlers.check_captcha)],
-}
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-conv_handler = ConversationHandler(
-    entry_points=[CommandHandler("start", handlers.start)],
-    states=states,
-    fallbacks=[],
-    name="main",
-    persistent=True,
-)
+# To repeat what the user said
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-dispatcher.add_handler(CommandHandler("list", handlers.get_list))
-dispatcher.add_handler(CommandHandler("stats", handlers.get_stats))
-dispatcher.add_handler(CommandHandler("bot", handlers.set_status))
-dispatcher.add_handler(conv_handler)
+echo_handler = MessageHandler(Filters.text and (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
 
-#Démarrez le bot
-if __name__ == "__main__":
+# To capitalize what the user inputted after /caps
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+# To captialize words using inline mode
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+# Unknown commands
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+# To start the bot
+if __name__ == '__main__':
     updater.start_polling()
-    updater.idle()
+    print("The bot is working.")
+
+# To end the bot
+updater.idle()
+print("The bot is stopped.")
